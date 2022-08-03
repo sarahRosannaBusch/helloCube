8e8b515d532dd0d0d357d16b380885086b44e897
Sarah Rosanna Busch <annarose0113@gmail.com>
2022-08-03 14:25:58 -0700

    solid primitives initial commit

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ var main = (function() {
         _initSpinningCubes();
         _initExtrudeHeart();
         _customShape();
+        _solidPrimitives();
     }
 
     function _initSpinningCubes() {
@@ -65,12 +66,7 @@ var main = (function() {
                 const canvas = renderer.domElement;
                 camera.aspect = canvas.clientWidth / canvas.clientHeight;
                 camera.updateProjectionMatrix();
-            }
-
-            //to make responsive (so blocks don't stretch)
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();   
+            }  
 
             cubes.forEach((cube, i) => {
                 if(turn !== i) return;
@@ -196,12 +192,7 @@ var main = (function() {
             }
 
             let offsetTop = elem.canvasD.offsetTop - window.pageYOffset;
-            heart.rotation.y = offsetTop * 0.01;
-
-            //to make responsive (so blocks don't stretch)
-            const canvas = renderer.domElement;
-            camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            camera.updateProjectionMatrix();   
+            heart.rotation.y = offsetTop * 0.01;   
         
             renderer.render(scene, camera);           
             requestAnimationFrame(render);
@@ -211,7 +202,7 @@ var main = (function() {
 
     function _customShape() {
         const canvas = f.html.getElem('#e');
-        const renderer = new THREE.WebGLRenderer({canvas});
+        const renderer = new THREE.WebGLRenderer({canvas, antialias: true}); //antialias smooths lines
 
         const fov = 40;
         const aspect = 2;  // the canvas default
@@ -219,7 +210,8 @@ var main = (function() {
         const far = 1000;
         const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
         camera.position.z = 40;
-        camera.position.y = 10;
+        camera.position.y = 0;
+        camera.position.x = -10;
 
         const scene = new THREE.Scene();
         scene.background = new THREE.Color(0x000000);
@@ -247,6 +239,7 @@ var main = (function() {
         const extrudeSettings = {
             steps: 2,  // ui: steps
             depth: 2.5,  // ui: depth
+            curveSegments: 30, //higher = smoother
             bevelEnabled: true,  // ui: bevelEnabled
             bevelThickness: 0.2,  // ui: bevelThickness
             bevelSize: 0.2,  // ui: bevelSize
@@ -256,8 +249,10 @@ var main = (function() {
         const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
         const material = new THREE.MeshPhongMaterial({color: 0x00ff00});           
         const puzzle = new THREE.Mesh(geometry, material);
-        puzzle.position.x = 0;
-        puzzle.position.y = 0;
+        puzzle.position.x = -10;
+        puzzle.position.y = -10;
+        puzzle.position.z = 10;
+        puzzle.rotation.x = -45;
         scene.add(puzzle); 
 
         //animate
@@ -271,7 +266,214 @@ var main = (function() {
                 camera.updateProjectionMatrix();
             }
 
-            puzzle.rotation.y = time;
+            //puzzle.rotation.y = time; 
+        
+            renderer.render(scene, camera);           
+            requestAnimationFrame(render);
+        }
+        requestAnimationFrame(render); //req to browser
+    }
+
+    function _solidPrimitives() {
+        const canvas = f.html.getElem('#f');
+        const renderer = new THREE.WebGLRenderer({canvas, antialias: true}); //antialias smooths lines
+
+        const fov = 40;
+        const aspect = 2;  // the canvas default
+        const near = 0.1;
+        const far = 1000;
+        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+        camera.position.z = 120;
+        camera.position.y = 0;
+        camera.position.x = -10;
+
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0x000000);
+        
+        renderer.render(scene, camera);
+
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
+
+        const objects = [];
+        const spread = 15;
+         
+        // Create mesh and add to scene
+        function addSolidGeometry(x, y, geometry) {
+            const mesh = new THREE.Mesh(geometry, createMaterial());
+            addObject(x, y, mesh);
+
+            function addObject(x, y, obj) {
+                obj.position.x = x * spread;
+                obj.position.y = y * spread;         
+                scene.add(obj);
+                objects.push(obj);
+              }
+      
+              function createMaterial() { //with random colour
+                  const material = new THREE.MeshPhongMaterial();           
+                  const hue = Math.random(); // 0=red .33=green .66=blue
+                  const saturation = 1; // 0=no colour
+                  const luminance = .5; // 0=black 1=white .5=max colour
+                  material.color.setHSL(hue, saturation, luminance);           
+                  return material;
+              }
+        }
+
+        function createBox(x, y) {
+            const width = 8;
+            const height = 8;
+            const depth = 8;
+            const geometry = new THREE.BoxGeometry(width, height, depth);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createCone(x, y) {
+            const radius = 6;  // ui: radius
+            const height = 8;  // ui: height
+            const radialSegments = 25;  // ui: radialSegments
+            const geometry = new THREE.ConeGeometry(radius, height, radialSegments);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createCylinder(x, y) {
+            const radiusTop = 4;  // ui: radiusTop
+            const radiusBottom = 4;  // ui: radiusBottom
+            const height = 8;  // ui: height
+            const radialSegments = 12;  // ui: radialSegments
+            const geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radialSegments);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createD12(x, y) {
+            const radius = 7;  // ui: radius
+            const geometry = new THREE.DodecahedronGeometry(radius);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createD20(x, y) {
+            const radius = 7;  // ui: radius
+            const geometry = new THREE.IcosahedronGeometry(radius);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createD8(x, y) {
+            const radius = 7;  // ui: radius
+            const geometry = new THREE.OctahedronGeometry(radius);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createD10(x, y) {
+            //vertices and faces taken from https://aqandrew.com/blog/10-sided-die-react/
+            const sides = 10;
+            const vertices = [
+              [0, 0, 1],
+              [0, 0, -1],
+            ].flat();
+          
+            for (let i = 0; i < sides; ++i) {
+              const b = (i * Math.PI * 2) / sides;
+              vertices.push(-Math.cos(b), -Math.sin(b), 0.105 * (i % 2 ? 1 : -1));
+            }
+
+            const faces = [
+                [0, 2, 3],
+                [0, 3, 4],
+                [0, 4, 5],
+                [0, 5, 6],
+                [0, 6, 7],
+                [0, 7, 8],
+                [0, 8, 9],
+                [0, 9, 10],
+                [0, 10, 11],
+                [0, 11, 2],
+                [1, 3, 2],
+                [1, 4, 3],
+                [1, 5, 4],
+                [1, 6, 5],
+                [1, 7, 6],
+                [1, 8, 7],
+                [1, 9, 8],
+                [1, 10, 9],
+                [1, 11, 10],
+                [1, 2, 11],
+            ].flat();
+
+            const radius = 7;  
+            const detail = 0;
+            const geometry = new THREE.PolyhedronGeometry(vertices, faces, radius, detail);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createSphere(x, y) {
+            const radius = 7;  
+            const widthSegments = 12;  
+            const heightSegments = 8;  
+            const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createD4(x, y) {
+            const radius = 7;  // ui: radius
+            const geometry = new THREE.TetrahedronGeometry(radius);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createTorus(x, y) {
+            const radius = 5;  // ui: radius
+            const tubeRadius = 2;  // ui: tubeRadius
+            const radialSegments = 8;  // ui: radialSegments
+            const tubularSegments = 24;  // ui: tubularSegments
+            const geometry = new THREE.TorusGeometry(
+                radius, tubeRadius,
+                radialSegments, tubularSegments);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        function createTorusKnot(x, y) {
+            const radius = 3.5;  // ui: radius
+            const tubeRadius = 1.5;  // ui: tubeRadius
+            const radialSegments = 8;  // ui: radialSegments
+            const tubularSegments = 64;  // ui: tubularSegments
+            const p = 2;  // ui: p
+            const q = 3;  // ui: q
+            const geometry = new THREE.TorusKnotGeometry(
+                radius, tubeRadius, tubularSegments, radialSegments, p, q);
+            addSolidGeometry(x, y, geometry);
+        }
+
+        createCone(-2, 2);
+        createSphere(0, 2);
+        createCylinder(2, 2);
+
+        createD4(-4, 0);
+        createBox(-2, 0);
+        createD8(0, 0);
+        createD12(2, 0);
+        createD20(4, 0);
+
+        createD10(-2, -2);
+        createTorus(0, -2);
+        createTorusKnot(2, -2);
+
+        //animate
+        function render(time) {
+            time *= 0.001;  // convert time to seconds  
+            
+            //to change resolution if necessary (for smooth edges)
+            if(_resizeRendererToDisplaySize(renderer)) {
+                const canvas = renderer.domElement;
+                camera.aspect = canvas.clientWidth / canvas.clientHeight;
+                camera.updateProjectionMatrix();
+            }
+
+            objects.forEach((obj, i) => {
+                obj.rotation.y = time;
+                obj.rotation.x = time;
+            });
 
             //to make responsive (so blocks don't stretch)
             const canvas = renderer.domElement;
